fcc067e34beb4104af89ddfc695afc8ed2acae4f
Jawdat Tayfour <18jwdat@gmail.com>
2023-07-01 18:29:34 +0300

    New updates for Heroku deplyment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9077xg&+wzfxk==*j+d!@=5qs$y5ywq$vzl)cjus9bs6ygwm($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
